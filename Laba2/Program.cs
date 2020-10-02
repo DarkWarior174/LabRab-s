cc5ac7e068889cb53eb476ddd6b815080efb5d63
Andrew <abris-chel@mail.ru>
2020-10-02 18:45:58 +0300

    NewRepo

---
+++ Laba2/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main()
+        {            
+            while (true)
+            {
+                string figureCoordinates;
+                bool isCorrect;
+                bool yourChoise;
+                int figure = 0;
+                string figureChoosing;
+
+                metka2:
+                Console.WriteLine("Пожалуйста, выбереите фигуру, где '1' - пешка, '2' - конь, \n" +
+                    "'3' - слон, '4' - ладья, '5' - король, '6' - ферзь.");
+                figureChoosing = Console.ReadLine();
+                int.TryParse(figureChoosing, out figure);
+
+                metka1:
+                Console.WriteLine("Пожалуйста, введите координаты фигуры до хода и после разделяя их пробелом");
+                figureCoordinates = Console.ReadLine();
+                isCorrect = IsEnterFigureCorrect(figureChoosing);
+                if (isCorrect)
+                {
+                    if (figure == 1)
+                    {
+                        figureChoosing = "пешкой";
+                    }
+                    else if (figure == 2)
+                    {
+                        figureChoosing = "конём";
+                    }
+                    else if (figure == 3)
+                    {
+                        figureChoosing = "слоном";
+                    }
+                    else if (figure == 4)
+                    {
+                        figureChoosing = "ладьёй";
+                    }
+                    else if (figure == 5)
+                    {
+                        figureChoosing = "королём";
+                    }
+                    else if (figure == 6)
+                    {
+                        figureChoosing = "ферзём";
+                    }
+
+                    isCorrect = IsEnteringCoordinatesCorrect(figureCoordinates);
+                    if (isCorrect)
+                    {
+                        yourChoise = ChoosingFigure(figureCoordinates, figure);
+                        if (yourChoise)
+                        {
+                            Console.WriteLine("А Вы мне нравитесь, такой ход " + figureChoosing + " возможен :)");
+                        }
+                        else
+                        {
+                            Console.WriteLine("К сожалению такой ход " + figureChoosing + " невозможен :(");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы неверно ввели координаты :(\nДавайте попробуем ещё раз :)\n");
+                        goto metka1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы не верно выбрали фигуру :(\nДавайте попробуем ещё раз :)\n");
+                    goto metka2;
+                }
+                Console.WriteLine("Просто нажмите любую кнопку, и мы попробуем ещё раз :)");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+
+        static bool IsEnteringCoordinatesCorrect(string strings)
+        {
+            byte lettersResult = 0;
+            bool nums = false;
+            bool result = false;
+            string lettersOnBoard = "ABCDEFGH";
+            char[] lettersChar = lettersOnBoard.ToCharArray();
+            if (strings.Length >= 5)
+            {
+                if (char.IsDigit(strings[1]) && char.IsDigit(strings[4]))
+                {
+                    if (char.GetNumericValue(strings[1]) <= 8 &&
+                        char.GetNumericValue(strings[4]) <= 8 &&
+                        char.GetNumericValue(strings[1]) != 0 &&
+                        char.GetNumericValue(strings[4]) != 0)
+                    {
+                        nums = true;
+                    }
+                }
+                for (int i = 0; i < 4;)
+                {
+                    for (int y = 0; y < 8;)
+                    {
+                        if (lettersChar[y] == strings[i])
+                        {
+                            lettersResult++;
+                        }
+                        y++;
+                    }
+                    i = i + 3;
+                }
+            }
+            if (nums && lettersResult == 2)
+            {
+                result = true;
+            }
+            return (result);
+        }
+
+        static bool IsEnterFigureCorrect(string strings)
+        {
+            bool result = false;
+            int figure;
+            int.TryParse(strings, out figure);
+            if (figure > 0 && figure < 7)
+            {
+                result = true;
+            }
+            return (result);
+        }
+        static int GetFigureCoordinates(string strings, int ID)
+        {
+            string lettersOnBoard = "ABCDEFGHIJ";
+            char[] lettersChar = lettersOnBoard.ToCharArray();
+            int coordinates = 0;
+            for (int y = 0; y <= 9;)
+            {
+                if (lettersChar[y] == strings[ID])
+                {
+                    coordinates = y;
+                }
+                y++;
+            }
+            return (coordinates);
+        }
+        static int GetFigureDelta(int a, int b)
+        {
+            int result = a - b;
+            return (Math.Abs(result));
+        }
+
+        static bool ChoosingFigure(string strings, int figureNumber)
+        {
+            bool result = false;
+            if (figureNumber == 1)
+            {
+                result = IsPawnMoveCorrect(strings);
+            }
+            else if (figureNumber == 2)
+            {
+                result = IsKnightMoveCorrect(strings);
+            }
+            else if (figureNumber == 3)
+            {
+                result = IsElephantMoveCorrect(strings);
+            }
+            else if (figureNumber == 4)
+            {
+                result = IsRookMoveCorrect(strings);
+            }
+            else if (figureNumber == 5)
+            {
+                result = IsKingMoveCorrect(strings);
+            }
+            else if (figureNumber == 6)
+            {
+                result = IsRookMoveCorrect(strings) || IsElephantMoveCorrect(strings);
+            }
+            return (result);
+        }
+
+        static bool IsRookMoveCorrect(string strings)
+        {
+            bool result = false;
+            int numberPCoordinate = Convert.ToInt32(strings[1]);
+            int numberACoordinate = Convert.ToInt32(strings[4]);
+            int letterPCoordinate = GetFigureCoordinates(strings, 0);
+            int letterACoordinate = GetFigureCoordinates(strings, 3);
+            int letterDelta = GetFigureDelta(letterPCoordinate, letterACoordinate);
+            int numberDelta = GetFigureDelta(numberPCoordinate, numberACoordinate);
+            if (letterDelta > 0 ^ numberDelta > 0)
+            {
+                result = true;
+            }
+            return (result);
+        }
+
+        static bool IsElephantMoveCorrect(string strings)
+        {
+            bool result = false;
+            int numberPCoordinate = Convert.ToInt32(strings[1]);
+            int numberACoordinate = Convert.ToInt32(strings[4]);
+            int letterPCoordinate = GetFigureCoordinates(strings, 0);
+            int letterACoordinate = GetFigureCoordinates(strings, 3);
+            int letterDelta = GetFigureDelta(letterPCoordinate, letterACoordinate);
+            int numberDelta = GetFigureDelta(numberPCoordinate, numberACoordinate);
+            if (letterDelta == numberDelta)
+            {
+                result = true;
+            }
+            return (result);
+        }
+
+        static bool IsKnightMoveCorrect(string strings)
+        {
+            bool result = false;
+            int numberPCoordinate = Convert.ToInt32(strings[1]);
+            int numberACoordinate = Convert.ToInt32(strings[4]);
+            int letterPCoordinate = GetFigureCoordinates(strings, 0);
+            int letterACoordinate = GetFigureCoordinates(strings, 3);
+            int letterDelta = GetFigureDelta(letterPCoordinate, letterACoordinate);
+            int numberDelta = GetFigureDelta(numberPCoordinate, numberACoordinate);
+            if (letterDelta <= 2 && numberDelta <= 2 && (letterDelta + numberDelta) == 3 )
+            {
+                result = true;
+            }
+            return (result);
+        }
+
+        static bool IsPawnMoveCorrect(string strings)
+        {
+            bool result = false;
+            int numberPCoordinate = Convert.ToInt32(strings[1]);
+            int numberACoordinate = Convert.ToInt32(strings[4]);
+            int letterPCoordinate = GetFigureCoordinates(strings, 0);
+            int letterACoordinate = GetFigureCoordinates(strings, 3);
+            int letterDelta = GetFigureDelta(letterPCoordinate, letterACoordinate);
+            int numberDelta = GetFigureDelta(numberPCoordinate, numberACoordinate);
+            if (letterDelta == 0 && numberDelta == 1)
+            {
+                result = true;
+            }
+            return (result);
+        }
+
+        static bool IsKingMoveCorrect(string strings)
+        {
+            bool result = false;
+            int numberPCoordinate = Convert.ToInt32(strings[1]);
+            int numberACoordinate = Convert.ToInt32(strings[4]);
+            int letterPCoordinate = GetFigureCoordinates(strings, 0);
+            int letterACoordinate = GetFigureCoordinates(strings, 3);
+            int letterDelta = GetFigureDelta(letterPCoordinate, letterACoordinate);
+            int numberDelta = GetFigureDelta(numberPCoordinate, numberACoordinate);
+            if (letterDelta <= 1 && numberDelta <= 1 && (letterDelta + numberDelta) == 1 || (letterDelta + numberDelta) == 2)
+            {
+                result = true;
+            }
+            return (result);
+        }
+    }
+}
---
+++ Laba3/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double delta = 0;
+            double startPos = 0;
+            double endPos = 0;
+            int maxLength = 0;
+            List<double> xCoordinates = new List<double>();
+            List<double> yCoordinates = new List<double>();
+            Console.WriteLine("Введите начальную координату");
+            startPos = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+            Console.WriteLine("Введите конечную координату");
+            endPos = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
+            Console.WriteLine("Введите альфу");
+            delta = Convert.ToDouble(Console.ReadLine().Replace('.', ',')); 
+            CheckCoordinates(startPos, endPos, delta);
+            xCoordinates = ListXGenerate(delta, startPos, endPos);
+            yCoordinates = ListYGenerate(xCoordinates);
+            int maxLengthX = StringLenght(xCoordinates);
+            int maxLengthY = StringLenght(yCoordinates);
+            if (maxLengthX > maxLengthY)
+                maxLength = maxLengthX;
+            else
+                maxLength = maxLengthY;
+            DrawingTable(xCoordinates, yCoordinates, maxLength);
+            Console.ReadKey();
+        } 
+        static void CheckCoordinates(double startPos, double endPos, double delta)
+        {
+
+            if (startPos == endPos || delta == 0)
+            {
+                Console.WriteLine("Неверно введены координаты");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            if ((startPos > endPos && delta > 0) || (startPos < endPos && delta < 0))
+            {
+                Console.WriteLine("Неверно введен шаг изменения! Невозможно достичь указанного значения!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+        static List<double> ListXGenerate(double delta, double startPos, double endPos)
+        {
+            List<double> xQueue = new List<double>();
+            xQueue.Add(startPos);
+            int itterations = Convert.ToInt32((endPos - startPos) / delta);
+            for (int i = 1; i <= itterations; i++)
+            {
+                xQueue.Add(startPos + (delta * i));
+            }
+            return xQueue;
+        }
+        static List<double> ListYGenerate(List<double> xQueue)
+        {
+            List<double> yQueue = new List<double>();
+            for (int i = 0; i < xQueue.Count; i++)
+            {
+                yQueue.Add(Math.Cos(xQueue[i]));
+            }
+            return yQueue;
+        }
+        static int StringLenght(List<double> coordinates)
+        {
+            int length;
+            int maxLength = 0;
+            string stringOfCoordintes;
+            for (int i = 0; i < coordinates.Count; i++)
+            {
+                stringOfCoordintes = Convert.ToString(coordinates[i]);
+                length = stringOfCoordintes.Length;
+                if (length > maxLength)
+                    maxLength = length;
+            }
+            return maxLength+4;
+        }
+        static void WiseChoice(char symbol, int itterations)
+        {
+            for (int i = 0; i < itterations; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+        static void DrawingMiddle(double queue, int maxLength)
+        {
+            string stringOfCoordintes = Convert.ToString(queue);
+            int length = stringOfCoordintes.Length;
+            int itterations = maxLength - length;
+            WiseChoice(' ', itterations / 2);
+            if (maxLength % 2 == 0 && length % 2 != 0)
+                Console.Write(" ");
+            else if (maxLength % 2 != 0 && length % 2 == 0)
+                Console.Write(" ");
+            Console.Write(queue);
+            WiseChoice(' ', itterations / 2);
+            Console.Write("|");
+        }
+        static void DrawingTop(char variable, int maxlength)
+        {
+            if (maxlength % 2 != 0)
+                Console.Write(' ');
+            WiseChoice(' ', maxlength / 2 - 1);
+            Console.Write(variable);
+            WiseChoice(' ', maxlength / 2 - 1);
+            Console.Write(" |");
+        }        
+        static void DrawingTable(List<double> xQueue, List<double> yQueue, int maxlength)
+        {
+            Console.Write("|");
+            for (int i = 0; i < 2; i++)
+            {
+                WiseChoice('-', maxlength);
+                Console.Write("|");
+            }
+            Console.WriteLine();
+            Console.Write("|");
+            DrawingTop('x', maxlength);
+            DrawingTop('y', maxlength);
+            Console.WriteLine();
+            for (int i = 0; i < 2 * maxlength + 3; i++)
+            {
+                Console.Write('-');
+            }
+            Console.WriteLine();
+            for (int i = 0; i < xQueue.Count; i++)
+            {
+                Console.Write("|");
+                DrawingMiddle(xQueue[i], maxlength);
+                DrawingMiddle(yQueue[i], maxlength);
+                Console.WriteLine();
+            }
+            Console.Write("|");
+            for (int i = 0; i < 2; i++)
+            {
+                WiseChoice('-', maxlength);
+                Console.Write("|");
+            }
+        }
+    }
+}
